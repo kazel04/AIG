50f67f3f37cfad167b7cda2261c66e55480bdaab
Kazel <59406000+kazel04@users.noreply.github.com>
2023-01-23 20:56:41 +0800

    Update HAL.py

--- HAL.py
+++ HAL.py
@@ -228,6 +228,39 @@ class World(object):
         
         return nearest_opponent
 
+    #get second nearest opponent
+    def get_second_nearest_opponent(self, char):
+        flag = False
+        second_nearest_opponent = None
+        distance = 0
+    
+        for entity in self.entities.values():
+    
+            # neutral entity
+            if entity.team_id == 2:
+                continue
+    
+            # same team
+            if entity.team_id == char.team_id:
+                continue
+    
+            if entity.name == "projectile" or entity.name == "explosion":
+                continue
+    
+            if entity.ko:
+                continue
+    
+            if second_nearest_opponent is None and flag == False: #if no target, set target here
+                flag = True
+            elif second_nearest_opponent is None and flag == True:
+                nearest_opponent = entity
+                distance = (char.position - entity.position).length()
+            else: #if target 
+                if distance > (char.position - entity.position).length():
+                    distance = (char.position - entity.position).length()
+                    second_nearest_opponent = entity
+        
+        return second_nearest_opponent
 
 
 class Obstacle(GameEntity):

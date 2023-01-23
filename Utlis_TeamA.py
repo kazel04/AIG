ff4ebb7d214f2574b87b0778f9c812874d852737
Leo Yun Tao <Leoyuntao@gmail.com>
2023-01-23 19:17:01 +0800

    try not to target through walls

--- Utlis_TeamA.py
+++ Utlis_TeamA.py
@@ -7,24 +7,24 @@ import numpy as np
 
 def is_opponent_in_range(character, opponent):
     
-    # x = [character.position.x, opponent.position.x]
-    # y = [character.position.y, opponent.position.y]
+    x = [character.position.x, opponent.position.x]
+    y = [character.position.y, opponent.position.y]
     
-    # equation = np.poly1d(np.polyfit(x, y, 1))
-    # x_axis = np.linspace(x[0], x[1], 10)
-    # y_axis = equation(x_axis)
+    equation = np.poly1d(np.polyfit(x, y, 1))
+    x_axis = np.linspace(x[0], x[1], 10)
+    y_axis = equation(x_axis)
     
-    # is_position_in_obstacle = False
+    is_position_in_obstacle = False
     
-    # for i in range(len(x_axis)):
-    #     predicted_pos = Vector2(x_axis[i], y_axis[i])
+    for i in range(len(x_axis)):
+        predicted_pos = Vector2(x_axis[i], y_axis[i])
 
-    #     dummy_character = GameEntity(character.world, "", pygame.image.load("assets/blue_archer_32_32.png").convert_alpha(), False)
-    #     dummy_character.rect = pygame.Rect(predicted_pos, (10, 10))
+        dummy_character = GameEntity(character.world, "", pygame.image.load("assets/blue_archer_32_32.png").convert_alpha(), False)
+        dummy_character.rect = pygame.Rect(predicted_pos, (10, 10))
         
-    #     if is_stuck(dummy_character):
-    #         is_position_in_obstacle = True
-    #         break
+        if is_stuck(dummy_character):
+            is_position_in_obstacle = True
+            break
     
     # if character.name == "archer":
     #     print(character.name + " " + str(is_position_in_obstacle))
